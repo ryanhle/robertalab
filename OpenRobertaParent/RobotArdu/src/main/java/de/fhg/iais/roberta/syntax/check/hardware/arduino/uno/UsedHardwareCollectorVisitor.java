fe8fe5168b041a5b0d4b19ca17b6c26134b83818
Evgeniya Ovchinnikova <elhed@yandex.ru>
2018-01-19 15:29:36 +0100

    #745 added base Uno files, configuration draft

--- OpenRobertaParent/RobotArdu/src/main/java/de/fhg/iais/roberta/syntax/check/hardware/arduino/uno/UsedHardwareCollectorVisitor.java
+++ OpenRobertaParent/RobotArdu/src/main/java/de/fhg/iais/roberta/syntax/check/hardware/arduino/uno/UsedHardwareCollectorVisitor.java
@@ -1,8 +1,10 @@
-package de.fhg.iais.roberta.syntax.check.hardware.arduino.botnroll;
+package de.fhg.iais.roberta.syntax.check.hardware.arduino.uno;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
-import de.fhg.iais.roberta.components.arduino.BotNrollConfiguration;
+import de.fhg.iais.roberta.components.UsedSensor;
 import de.fhg.iais.roberta.syntax.Phrase;
 import de.fhg.iais.roberta.syntax.actors.arduino.mbot.ExternalLedOffAction;
 import de.fhg.iais.roberta.syntax.actors.arduino.mbot.ExternalLedOnAction;
@@ -19,9 +21,15 @@ import de.fhg.iais.roberta.visitors.arduino.ArduinoAstVisitor;
  */
 public class UsedHardwareCollectorVisitor extends RobotUsedHardwareCollectorVisitor implements ArduinoAstVisitor<Void> {
 
-    public UsedHardwareCollectorVisitor(ArrayList<ArrayList<Phrase<Void>>> phrasesSet, BotNrollConfiguration configuration) {
-        super(configuration);
+    protected final Set<UsedSensor> usedSensors = new LinkedHashSet<>();
+
+    public UsedHardwareCollectorVisitor(ArrayList<ArrayList<Phrase<Void>>> phrasesSet) {
+        super(null);
         check(phrasesSet);
+    }
+
+    public Set<UsedSensor> getTimer() {
+        return this.usedSensors;
     }
 
     @Override
